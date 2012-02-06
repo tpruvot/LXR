860469d73070475abf48c37525203536cbe445c3
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-02-06 23:18:08 +0100

    LXR Config and Template

--- lxr/lib/LXR/Config.pm
+++ lxr/lib/LXR/Config.pm
@@ -1,6 +1,6 @@
 # -*- tab-width: 4 -*- ###############################################
 #
-# $Id: Config.pm,v 1.41 2011/12/30 10:51:10 ajlittoz Exp $
+# $Id: Config.pm,v 1.46 2012/02/04 16:31:56 ajlittoz Exp $
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 
 package LXR::Config;
 
-$CVSID = '$Id: Config.pm,v 1.41 2011/12/30 10:51:10 ajlittoz Exp $ ';
+$CVSID = '$Id: Config.pm,v 1.46 2012/02/04 16:31:56 ajlittoz Exp $ ';
 
 use strict;
 use File::Path;
@@ -35,9 +35,29 @@ sub new {
 	my ($class, @parms) = @_;
 	my $self = {};
 	bless($self);
-	$self->_initialize(@parms);
-	return ($self);
-	die("Foo!\n");
+	if ($self->_initialize(@parms)) {
+		return ($self);
+	} else {
+		return undef;
+	}
+}
+
+sub readconfig {
+	my $self = shift;
+	my $confpath = $$self{'confpath'};
+
+	unless (open(CONFIG, $confpath)) {
+		die("Couldn't open configuration file \"$confpath\".");
+	}
+
+	local ($/) = undef;
+	my $config_contents = <CONFIG>;
+	$config_contents =~ /(.*)/s;
+	$config_contents = $1;    #untaint it
+	my @config = eval("\n#line 1 \"configuration file\"\n" . $config_contents);
+	die($@) if $@;
+
+	return wantarray ? @config : $config[0];
 }
 
 sub readfile {
@@ -90,8 +110,12 @@ sub _initialize {
 		%$self = (%$self, %{ $config[0] });
 	}
 
-	$url =~ m!(.*?://.*?)/!;	# host name and port used to access server
+	$url =~ m!(^.*?://.*?)/!;	# host name and port used to access server
 	my $host = $1;
+		# To allow simultaneous Apache and lighttpd operation
+		# on 2 different ports, remove port for identification
+	$host =~ s/(:\d+|)$//;
+	my $port = $1;
 	my $script_path;
 	if ($url) {
 		($script_path = $url) =~ s!^.*?://[^/]*!!; # remove host and port
@@ -115,15 +139,21 @@ sub _initialize {
 			foreach my $rt (@hostnames) {
 				$rt =~ s!/*$!!;		# remove trailing /
 				$rt =~ s!^//!http://!; # allow for a shortened form
+		# To allow simultaneous Apache and lighttpd operation
+		# on 2 different ports, remove port for identification
+				$rt =~ s/:\d+$//;
 				if	(	$host eq $rt
 					&&	$script_path eq $virtroot
 					) {
-					$config->{'baseurl'} = $rt . $script_path;
+					$config->{'baseurl'} = $rt . $port . $script_path;
 					%$self = (%$self, %$config);
 					last CANDIDATE;
 				}
 			}
 		} else { # elsif ($config->{'baseurl'}) {
+		# To allow simultaneous Apache and lighttpd operation
+		# on 2 different ports, remove port for identification
+			$url =~ s/:\d+$//;
 			my @aliases;
 			if ($config->{'baseurl_aliases'}) {
 				@aliases = @{ $config->{'baseurl_aliases'} };
@@ -132,8 +162,10 @@ sub _initialize {
 			push @aliases, $root;
 			foreach my $rt (@aliases) {
 				$rt .= '/' unless $rt =~ m#/$#;    # append / if necessary
+				$rt =~ s/:\d+$//;	# remove port (approximate match)
 				my $r = quotemeta($rt);
 				if ($url =~ /^$r/) {
+					$rt =~ s/^$r/$rt$port/;
 					$config->{'baseurl'} = $rt;
 					%$self = (%$self, %$config);
 					last CANDIDATE;
@@ -142,19 +174,44 @@ sub _initialize {
 		}
 	}
 
-	$$self{'encoding'} = "iso-8859-1" unless (exists $self->{'encoding'});
-
 	if(!exists $self->{'baseurl'}) {
 		if("genxref" ne ($0 =~ /([^\/]*)$/)) {
-			return 1;
+			return 0;
 		}
 		elsif($url =~ m!https?://.+\.!) {
 			die "Can't find config for $url: make sure there is a 'baseurl' line that matches in lxr.conf\n";
-		} else {	
+		} else {
 			# wasn't a url, so probably genxref with a bad --url parameter
 			die "Can't find config for $url: " . 
 			 	"the --url parameter should be a URL (e.g. http://example.com/lxr) and must match a baseurl line in lxr.conf\n";
 		}
+	}
+
+	$$self{'encoding'} = "iso-8859-1" unless (exists $self->{'encoding'});
+
+	if (!exists $self->{'filetype'}) {
+		if (exists $self->{'filetypeconf'}) {
+			unless (open(FILETYPE, $self->{'filetypeconf'})) {
+				die("Couldn't open configuration file ".$self->{'filetypeconf'});
+			}
+			local ($/) = undef;
+			my $contents = <FILETYPE>;
+			$contents =~ /(.*)/s;
+			$contents = $1;    #untaint it
+			my $mapping = eval("\n#line 1 \"file mappings\"\n" . $contents);
+			die($@) if $@;
+			if (defined($mapping)) {
+				%$self = (%$self, %$mapping);
+			}
+		}
+	}
+	if (!exists $self->{'filetype'}) {
+		die "No file type mapping in $confpath.\n"
+			. "Please specify 'filetype' or 'filetypeconf' \n";
+	}
+	if (!exists $self->{'interpreters'}) {
+		die "No script interpreter mapping in $confpath.\n"
+			. "Please specify 'interpreters' or 'filetypeconf' \n";
 	}
 
 	# Set-up various directories as necessary
@@ -173,6 +230,7 @@ sub _initialize {
 		die "Neither Glimpse nor Swish have been specified in $confpath.\n".
 		"Please choose one of them by specifing a value for either glimpsebin or swishbin.\n";
 	}
+	return 1;
 }
 
 sub allvariables {
@@ -197,7 +255,7 @@ sub vardefault {
 	}
 	if (ref($self->{variables}{$var}{range}) eq "CODE") {
 		my @vr = varrange($var);
-		return $vr[0] if scalar(@vr); return "head"
+		return $vr[0] if scalar(@vr)>0; return "head"
 	}
 	return	$self->{variables}{$var}{range}[0];
 }
