3ec941f52253dca476bed95d2d90ae74dd3cc077
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-02-09 12:47:05 +0100

    reduce local diff

--- lxr/apache2-require.pl
+++ lxr/apache2-require.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl -w
 @INC=	( @INC
-	, "."		# <- LXR root directory
-	, "./lib"	# <- LXR library directory
-	);
+		, "."		# <- LXR root directory
+		, "./lib"	# <- LXR library directory
+		);
 
 1;
